a878c06af16eaadc46e5970fb9580b66274de087
huuthien <huuthienit.2018@gmail.com>
2025-04-17 14:34:26 +0700

    update port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9999;
 
 app.use(bodyParser.json());
 
